ae54bbf64766be35aade12cdf66e71aedeb12c44
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-07-14 22:55:26 +0200

    Create tip_model.dart

---
+++ lib/models/tip_model.dart
@@ -0,0 +1,13 @@
+class Tipp {
+  final String hazaiCsapat;
+  final String vendegCsapat;
+  final String tippeltEredmeny;
+  final double odds;
+
+  Tipp({
+    required this.hazaiCsapat,
+    required this.vendegCsapat,
+    required this.tippeltEredmeny,
+    required this.odds,
+  });
+}
